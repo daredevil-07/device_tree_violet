08648f1249eea9219803e3c4d2256cb9b076f9f7
// S∆UR∆V // <57617226+ugly-kid-af@users.noreply.github.com>
2020-01-13 19:27:07 +0530

    Update and rename carbon_violet.mk to aosp_violet.mk

--- aosp_violet.mk
+++ aosp_violet.mk
@@ -18,11 +18,11 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
-$(call inherit-product, vendor/carbon/config/common.mk)
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := carbon_violet
+PRODUCT_NAME := aosp_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
